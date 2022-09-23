0d9ff2b37c6cedae16b8c1c761c93468bf84f065
Jeffrey Peake <jp276297@gmail.com>
2022-09-23 16:40:05 +1200

    Added Firebase initializing file and dependencys

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBx5tthRrnsBWbmEKdpDOqao-oTZAa851w",
+
+  authDomain: "nwen304-rest-app.firebaseapp.com",
+
+  projectId: "nwen304-rest-app",
+
+  storageBucket: "nwen304-rest-app.appspot.com",
+
+  messagingSenderId: "852370644536",
+
+  appId: "1:852370644536:web:a9b5224f4b0de9cd7650cd"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
