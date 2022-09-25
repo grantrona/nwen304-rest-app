64761e018d339cad05eb094c5df74a25d8bb2048
Jeffrey Peake <jp276297@gmail.com>
2022-09-26 12:11:49 +1300

    Added public folder and simple static css file

--- webapp/index.js
+++ webapp/index.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const express = require('express');
 const userRoutes = require('./routes/UserRoutes');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(userRoutes);
 
 const server = http.createServer(app);
