c21774a59959faa0669d3a5a5922a402f0a7e31f
Jeffrey Peake <jp276297@gmail.com>
2022-09-26 11:44:27 +1300

    Refactored webapp to use express routes, index.js defers routing to UserRoutes currently, which serves index.html

--- webapp/index.js
+++ webapp/index.js
@@ -1,19 +1,10 @@
 const http = require('http');
 const express = require('express');
+const userRoutes = require('./routes/UserRoutes');
 
 const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello There');
-});
+app.use(userRoutes);
 
-
-const hostname = '127.0.0.1';
-const port = 3001;
-const serverFunction = () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-}
-
-server.listen(port, hostname, serverFunction);
+const server = http.createServer(app);
+server.listen(3000);
