9d91f353f1dd1712b4cee5bed29d0aff43c40e89
DilithaW123 <diliw101@gmail.com>
2022-09-26 19:19:00 +1300

    Working on cookies

--- webapp/routes/AuthRoutes.js
+++ webapp/routes/AuthRoutes.js
@@ -82,4 +82,6 @@ router.post('/logout',(req,res) => {
 
 });
 
+
+
 module.exports = router;
--- webapp/routes/UserRoutes.js
+++ webapp/routes/UserRoutes.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Any http request will be handled by rendering index.ejs:
-router.use('/', (req, res) => {
-    //Render index.ejs, passing the variable 'ExampleVariable'
-    res.render('index', {'ExampleVariable' : 'wow'})
-});
-
 // Direct user to login page
 router.use('/pages/login', (req, res) => {
     res.render('loginPage');
@@ -15,6 +9,12 @@ router.use('/pages/login', (req, res) => {
 // Direct user to sign up page
 router.use('/pages/signup', (req, res) => {
     res.render('signupPage');
+});
+
+// Any http request will be handled by rendering index.ejs:
+router.use('/', (req, res) => {
+    //Render index.ejs, passing the variable 'ExampleVariable'
+    res.render('index', {'ExampleVariable' : 'wow'})
 });
 
 module.exports=router
