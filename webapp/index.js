f515618fa644d6e9dab185b246f0fd2c1d3847b1
Dilitha Wijayapala <60630994+DilithaW123@users.noreply.github.com>
2022-09-26 13:00:51 +1300

    Update index.js

--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,39 @@
 const http = require('http');
 const express = require('express');
-const userRoutes = require('./routes/UserRoutes');
-const path = require('path');
+const auth = require('./routes/auth');
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
+require('firebase/app');
+const { getFirestore } = require('firebase/firestore');
+const firebaseConfig = {
+  apiKey: "AIzaSyBx5tthRrnsBWbmEKdpDOqao-oTZAa851w",
+  authDomain: "nwen304-rest-app.firebaseapp.com",
+  projectId: "nwen304-rest-app",
+  storageBucket: "nwen304-rest-app.appspot.com",
+  messagingSenderId: "852370644536",
+  appId: "1:852370644536:web:a9b5224f4b0de9cd7650cd"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser)
+app.use('/',auth);
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(userRoutes);
+// TODO: Send index.ejs file
+app.get('/',(req, res) => {
+  res.send("Hello from server")
+})
 
-const server = http.createServer(app);
-server.listen(3000);
+const HOSTNAME = '127.0.0.1';
+const PORT = process.env.port || 3001;
+const serverFunction = () => {
+    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+}
+
+server.listen(PORT, HOSTNAME, serverFunction);
+module.exports = db;
