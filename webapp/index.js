f42403d29a0e0e3caf694b18c246868cc146c9f4
Jeffrey Peake <jp276297@gmail.com>
2022-09-26 13:43:06 +1300

    Implemented ejs template engine

--- webapp/index.js
+++ webapp/index.js
@@ -5,8 +5,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+// Setup ejs template engine:
+app.set("view engine", 'ejs');
+app.set('views', 'views'); // point to 'views' folder
+
+// specify static files and routes to use:
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(userRoutes);
 
+// Create server and listen on port 3000:
 const server = http.createServer(app);
 server.listen(3000);
--- webapp/routes/UserRoutes.js
+++ webapp/routes/UserRoutes.js
@@ -1,9 +1,10 @@
-const path = require('path')
 const express = require('express');
 const router = express.Router();
 
+// Any http request will be handled by rendering index.ejs:
 router.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+    //Render index.ejs, passing the variable 'ExampleVariable'
+    res.render('index', {'ExampleVariable' : 'wow'})
 });
 
 module.exports=router
