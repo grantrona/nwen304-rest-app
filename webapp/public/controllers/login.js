a5dc85cae9adfa4c91a47ab9d59e2cf117df3502
DilithaW123 <diliw101@gmail.com>
2022-09-26 17:00:48 +1300

    Basic logic pages

---
+++ webapp/public/controllers/login.js
@@ -0,0 +1,24 @@
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passwordInput').value;
+    if (email.trim() === '' || password.trim() === '') {
+        alert('Fill in all the fields');
+        return;
+    }
+    console.log("here");
+    fetch("/login/email", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email,
+            password
+        }),
+    })
+    .then(() => {
+        console.log('sent request');
+    })
+})
