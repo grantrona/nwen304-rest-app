9595e59d2dcb74dcaeeaea9222f46dfcbd59383a
Jeffrey Peake <jp276297@gmail.com>
2022-09-29 12:24:02 +1300

    Index page now gets posts from firestore, Of concern however is the length of time to retrieve posts, Nothing is sent to the client until the posts are recieved.

--- webapp/routes/UserRoutes.js
+++ webapp/routes/UserRoutes.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+const { db } = require('../utils/firebaseConfig');
+const { doc, getDocs, collection, getDoc } = require('firebase/firestore');
 
 // Direct user to login page
 router.use('/login', (req, res) => {
-    // res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
     res.render('login', {isAuth: req.cookies['session']? true: false})
 });
 
 // Direct user to sign up page
 router.use('/signup', (req, res) => {
-    // res.sendFile(path.join(__dirname, '..', 'public', 'register.html'));
     res.render('register', {isAuth: req.cookies['session']? true: false})
 });
 
 // Navigate to add post html file
 router.get('/addPost', (req, res) =>{
-    // res.sendFile(path.join(__dirname, '..', 'public', 'add-post.html'));
     res.render('add-post', {isAuth: req.cookies['session']? true: false})
 })
 
+/** Temporarily in UserRoutes, should be moved to util perhaps? */
+async function getPosts(){
+    const posts = [];
+    const postsSnapshot = await getDocs(collection(db, "posts"));
+    postsSnapshot.forEach((postDoc) =>{
+        posts.push(postDoc.data())
+    });
+    return posts
+}
 
 router.use('/', (req, res) => {
-    const posts = [
-        {
-            title: "testTitle",
-            content: "testContent"
-        },
-        {
-            title: "Another title",
-            content: "Another Content"
-        }
-    ]; // TODO: get list of posts from database.
-    res.render('index', {posts : posts, isAuth: req.cookies['session']? true: false})
+    const isAuth = req.cookies['session']? true: false;
+    getPosts().then((posts)=> {
+        res.render('index', {posts : posts, isAuth: isAuth})
+    });
 });
 
 module.exports=router
