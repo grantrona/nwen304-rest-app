352993c66fe101dec2534a24261c0b084783db73
DilithaW123 <diliw101@gmail.com>
2022-10-02 02:50:37 +1300

    Removed unnecessary variables from ejs files

--- webapp/routes/ProtectedRoutes.js
+++ webapp/routes/ProtectedRoutes.js
@@ -17,7 +17,26 @@ router.post('/protected/addPost', (req, res) =>{
 }); 
 
 router.get('/protected/myposts', (req, res) => {
-
+    let creatorID = jwt.decode(req.cookies['session'], secret_key).id;
+    const timeStampFormat = 'MM/DD/YY hh:mm:ss A';
+    const userQuery = query(collection(db, "posts"), where("creatorID", "==", req.query.creatorID));
+    getDocs(userQuery)
+    .then(postsSnapshot=> {
+        let posts = [];
+        postsSnapshot.forEach(postDoc => {
+            posts.push({...postDoc.data(), id: postDoc.id});
+        });
+        posts.sort((a, b) => {
+            const aMoment = new moment(a.date, timeStampFormat);
+            const bMoment = new moment(b.date, timeStampFormat);
+            return bMoment.diff(aMoment);
+        });
+        res.render('user-posts', {posts: posts, isAuth: true});
+    })
+    .catch((err)=> {
+        res.sendStatus(400);
+        console.log(err);
+    })
 });
 
 module.exports = router;
